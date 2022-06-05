9cdfce5690a395f1cf10f646d73a5ee91272711b
Chayse Stiver <chayse.stiver@gmail.com>
2022-06-05 12:54:37 -0700

    changed nav bar font size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,16 +6,16 @@ const Navbar: React.FC = () => {
     <nav className={style.nav}>
       <div className={style.navbuttons}>
         <Link href="/">
-          <a className={style.btn}>00. Home</a>
+          <a className={style.btn}>Home</a>
         </Link>
         <Link href="/skills">
-          <a className={style.btn}>01. Skills</a>
+          <a className={style.btn}>Skills</a>
         </Link>
         <Link href="/work">
-          <a className={style.btn}>02. Work</a>
+          <a className={style.btn}>Work</a>
         </Link>
         <Link href="/contact">
-          <a className={style.btn}>03. Contact</a>
+          <a className={style.btn}>Contact</a>
         </Link>
       </div>
     </nav>
