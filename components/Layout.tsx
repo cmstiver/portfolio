930f98e6a754cbbb76c26d7c274306d8614e9ffe
Chayse Stiver <chayse.stiver@gmail.com>
2022-06-05 13:36:41 -0700

    change favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,7 @@ const Layout: React.FC<Props> = ({ children }) => {
           name="viewport"
           content="width=device-width, initial-scale=1"
         ></meta>
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <Navbar />
       <div>{children}</div>
