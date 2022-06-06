59ac2f0e0b783ef99973a200eec4909760a6348f
Chayse Stiver <chayse.stiver@gmail.com>
2022-06-05 23:35:27 -0700

    contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,9 +4,52 @@ import style from "../styles/Contact.module.scss";
 const Contact: NextPage = () => {
   return (
     <div className={style.contact}>
-      <div>I&apos;d love to hear from you!</div>
-      <div>Please contact me at:</div>
-      <div className={style.email}>chayse.stiver@gmail.com</div>
+      <form
+        acceptCharset="UTF-8"
+        action="https://getform.io/f/53b6a1db-6017-4fd7-bea4-f63c591c6de7"
+        method="POST"
+        encType="multipart/form-data"
+        target="_blank"
+        id="wpform"
+      >
+        <div className={style.labelinput}>
+          <label>Name</label>
+          <div>
+            <input
+              className={style.input}
+              type="text"
+              name="name"
+              placeholder="Name"
+              required={true}
+            />
+          </div>
+        </div>
+        <div className={style.labelinput}>
+          <label>Email Address</label>
+          <div>
+            <input
+              className={style.input}
+              type="text"
+              name="email"
+              placeholder="Email"
+              required={true}
+            />
+          </div>
+        </div>
+        <div className={style.labelinput}>
+          <label>Your Message:</label>
+          <div>
+            <textarea
+              className={style.inputbox}
+              name="message"
+              required={true}
+            />
+          </div>
+        </div>
+        <button className={style.button} type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
